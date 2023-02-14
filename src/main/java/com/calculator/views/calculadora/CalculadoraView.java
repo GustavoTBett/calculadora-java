8f3ac759a1112f86cd747ca9a8f7ac2b2c22594c
D41K1 <gustavotaufembachbtt@gmail.com>
2023-02-13 22:56:01 -0300

    Estilização

--- src/main/java/com/calculator/views/calculadora/CalculadoraView.java
+++ src/main/java/com/calculator/views/calculadora/CalculadoraView.java
@@ -2,6 +2,7 @@ package com.calculator.views.calculadora;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -11,9 +12,6 @@ import com.vaadin.flow.router.Route;
 public class CalculadoraView extends VerticalLayout {
 
     public CalculadoraView() {
-        Div teste = new Div();
-        teste.add("=");
-
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
@@ -25,7 +23,7 @@ public class CalculadoraView extends VerticalLayout {
         add(layout);
         layout.setClassName("layout");
 
-        VerticalLayout display = new VerticalLayout();
+        Div display = new Div();
         Div buttons = new Div();
         layout.add(display);
         layout.add(buttons);
@@ -33,14 +31,13 @@ public class CalculadoraView extends VerticalLayout {
         buttons.setClassName("buttons");
 
         VerticalLayout historico = new VerticalLayout();
-        VerticalLayout tela = new VerticalLayout();
+        Div tela = new Div();
         display.add(historico);
         display.add(tela);
         historico.setClassName("historico");
         tela.setClassName("tela");
 
         Div historicoTextField = new Div();
-        historicoTextField.add("1 + 1");
         historico.add(historicoTextField);
 
         VerticalLayout modulo = new VerticalLayout();
@@ -52,33 +49,29 @@ public class CalculadoraView extends VerticalLayout {
 
         Div moduloTextField = new Div();
         Div resultadoTextField = new Div();
-        moduloTextField.add("=");
-        resultadoTextField.add("");
         modulo.add(moduloTextField);
         resultado.add(resultadoTextField);
 
-        Button ce = new Button("CE");
-        buttons.add(ce);
-        ce.setClassName("ce");
-        ce.addClickListener(clickEvent -> {
+        Image imagemCe = new Image("images/CE.png", "CE");
+        Button ce = new Button(imagemCe, clickEvent -> {
             // apaga o número
             resultadoTextField.removeAll();
         });
+        buttons.add(ce);
+        ce.setClassName("ce");
 
-        Button c = new Button("C");
-        buttons.add(c);
-        c.setClassName("ce");
-        c.addClickListener(clickEvent -> {
+        Image imagemC = new Image("images/C.png", "C");
+        Button c = new Button(imagemC, clickEvent -> {
             // apaga toda a operação
             resultadoTextField.removeAll();
             moduloTextField.removeAll();
             historicoTextField.removeAll();
         });
+        buttons.add(c);
+        c.setClassName("ce");
 
-        Button porcentagem = new Button("%");
-        buttons.add(porcentagem);
-        porcentagem.setClassName("ce");
-        porcentagem.addClickListener(clickEvent -> {
+        Image imagemPorcentagem = new Image("images/Percent.png", "Porcentagem");
+        Button porcentagem = new Button(imagemPorcentagem, clickEvent -> {
             moduloTextField.removeAll();
             var hist = resultadoTextField.getText();
             moduloTextField.add("%");
@@ -86,11 +79,11 @@ public class CalculadoraView extends VerticalLayout {
             historicoTextField.add(hist);
             resultadoTextField.removeAll();
         });
+        buttons.add(porcentagem);
+        porcentagem.setClassName("ce");
 
-        Button divisao = new Button("/");
-        buttons.add(divisao);
-        divisao.setClassName("modulos");
-        divisao.addClickListener(clickEvent -> {
+        Image imagemDivide = new Image("images/Divide.png", "divide");
+        Button divisao = new Button(imagemDivide, clickEvent -> {
             moduloTextField.removeAll();
             var hist = resultadoTextField.getText();
             moduloTextField.add("/");
@@ -98,32 +91,26 @@ public class CalculadoraView extends VerticalLayout {
             historicoTextField.add(hist);
             resultadoTextField.removeAll();
         });
+        buttons.add(divisao);
+        divisao.setClassName("modulos");
 
-        Button sete = new Button("7");
+        Image imagemSete = new Image("images/7.png", "7");
+        Button sete = new Button(imagemSete, clickEvent -> resultadoTextField.add("7"));
         buttons.add(sete);
         sete.setClassName("ce");
-        sete.addClickListener(clickEvent -> {
-            resultadoTextField.add("7");
-        });
 
-        Button oito = new Button("8");
+        Image imagemOito = new Image("images/8.png", "8");
+        Button oito = new Button(imagemOito, clickEvent -> resultadoTextField.add("8"));
         buttons.add(oito);
         oito.setClassName("ce");
-        oito.addClickListener(clickEvent -> {
-            resultadoTextField.add("8");
-        });
 
-        Button nove = new Button("9");
+        Image imagemNove = new Image("images/9.png", "9");
+        Button nove = new Button(imagemNove, clickEvent -> resultadoTextField.add("9"));
         buttons.add(nove);
         nove.setClassName("ce");
-        nove.addClickListener(clickEvent -> {
-            resultadoTextField.add("9");
-        });
 
-        Button multiplicacao = new Button("X");
-        buttons.add(multiplicacao);
-        multiplicacao.setClassName("modulos");
-        multiplicacao.addClickListener(clickEvent -> {
+        Image imagemMulti = new Image("images/X.png", "multi");
+        Button multiplicacao = new Button(imagemMulti, clickEvent -> {
             moduloTextField.removeAll();
             var hist = resultadoTextField.getText();
             moduloTextField.add("X");
@@ -131,32 +118,29 @@ public class CalculadoraView extends VerticalLayout {
             historicoTextField.add(hist);
             resultadoTextField.removeAll();
         });
+        buttons.add(multiplicacao);
+        multiplicacao.setClassName("modulos");
 
-        Button quatro = new Button("4");
+        Image imagemQuatro = new Image("images/4.png", "4");
+        Button quatro = new Button(imagemQuatro);
         buttons.add(quatro);
         quatro.setClassName("ce");
         quatro.addClickListener(clickEvent -> {
             resultadoTextField.add("4");
         });
 
-        Button cinco = new Button("5");
+        Image imagemCinco = new Image("images/5.png", "5");
+        Button cinco = new Button(imagemCinco, clickEvent -> resultadoTextField.add("5"));
         buttons.add(cinco);
         cinco.setClassName("ce");
-        cinco.addClickListener(clickEvent -> {
-            resultadoTextField.add("5");
-        });
 
-        Button seis = new Button("6");
+        Image imagemSeis = new Image("images/6.png", "6");
+        Button seis = new Button(imagemSeis, clickEvent -> resultadoTextField.add("6"));
         buttons.add(seis);
         seis.setClassName("ce");
-        seis.addClickListener(clickEvent -> {
-            resultadoTextField.add("6");
-        });
 
-        Button menos = new Button("-");
-        buttons.add(menos);
-        menos.setClassName("modulos");
-        menos.addClickListener(clickEvent -> {
+        Image imagemMenos = new Image("images/Minus.png", "menos");
+        Button menos = new Button(imagemMenos, clickEvent -> {
             moduloTextField.removeAll();
             var hist = resultadoTextField.getText();
             moduloTextField.add("-");
@@ -164,32 +148,26 @@ public class CalculadoraView extends VerticalLayout {
             historicoTextField.add(hist);
             resultadoTextField.removeAll();
         });
+        buttons.add(menos);
+        menos.setClassName("modulos");
 
-        Button um = new Button("1");
+        Image imagemUm = new Image("images/1.png", "1");
+        Button um = new Button(imagemUm, clickEvent -> resultadoTextField.add("1"));
         buttons.add(um);
         um.setClassName("ce");
-        um.addClickListener(clickEvent -> {
-            resultadoTextField.add("1");
-        });
 
-        Button dois = new Button("2");
+        Image imagemDois = new Image("images/2.png", "2");
+        Button dois = new Button(imagemDois, clickEvent -> resultadoTextField.add("2"));
         buttons.add(dois);
         dois.setClassName("ce");
-        dois.addClickListener(clickEvent -> {
-            resultadoTextField.add("2");
-        });
 
-        Button tres = new Button("3");
+        Image imagemTres = new Image("images/3.png", "3");
+        Button tres = new Button(imagemTres, clickEvent -> resultadoTextField.add("3"));
         buttons.add(tres);
         tres.setClassName("ce");
-        tres.addClickListener(clickEvent -> {
-            resultadoTextField.add("3");
-        });
 
-        Button mais = new Button("+");
-        buttons.add(mais);
-        mais.setClassName("modulos");
-        mais.addClickListener(clickEvent -> {
+        Image imagemMais = new Image("images/Plus.png", "mais");
+        Button mais = new Button(imagemMais, clickEvent -> {
             moduloTextField.removeAll();
             var hist = resultadoTextField.getText();
             moduloTextField.add("+");
@@ -197,33 +175,32 @@ public class CalculadoraView extends VerticalLayout {
             historicoTextField.add(hist);
             resultadoTextField.removeAll();
         });
+        buttons.add(mais);
+        mais.setClassName("modulos");
 
-        Button maisOuMenos = new Button("+/-");
-        buttons.add(maisOuMenos);
-        maisOuMenos.setClassName("ce");
-        maisOuMenos.addClickListener(clickEvent -> {
+        Image imagemMaisOuMenos = new Image("images/PlusMinus.png", "MaisOuMenos");
+        Button maisOuMenos = new Button(imagemMaisOuMenos, clickEvent -> {
             var teste2 = resultadoTextField.getText();
             resultadoTextField.removeAll();
-            float number = Float.parseFloat(teste2) * (-1);
+            double number = Double.parseDouble(teste2) * (-1);
             String s = String.valueOf(number);
             resultadoTextField.add(s);
         });
+        buttons.add(maisOuMenos);
+        maisOuMenos.setClassName("ce");
 
-        Button zero = new Button("0");
+        Image imagemZero = new Image("images/0.png", "0");
+        Button zero = new Button(imagemZero, clickEvent -> resultadoTextField.add("0"));
         buttons.add(zero);
         zero.setClassName("ce");
-        zero.addClickListener(clickEvent -> {
-            resultadoTextField.add("0");
-        });
 
-        Button virgula = new Button(",");
+        Image imagemVirgula = new Image("images/,.png", "virgúla");
+        Button virgula = new Button(imagemVirgula, clickEvent -> resultadoTextField.add("."));
         buttons.add(virgula);
         virgula.setClassName("ce");
-        virgula.addClickListener(clickEvent -> {
-            resultadoTextField.add(".");
-        });
 
-        Button igual = new Button("=");
+        Image imagemIgual = new Image("images/Equals.png", "igual");
+        Button igual = new Button(imagemIgual);
         buttons.add(igual);
         igual.setClassName("igual");
         igual.addClickListener(clickEvent -> {
@@ -231,8 +208,8 @@ public class CalculadoraView extends VerticalLayout {
 
             var histResult = historicoTextField.getText();
             var y = resultadoTextField.getText();
-            float number = Float.parseFloat((histResult));
-            float number2 = Float.parseFloat((y));
+            double number = Double.parseDouble((histResult));
+            double number2 = Double.parseDouble((y));
 
             historicoTextField.removeAll();
             historicoTextField.add(histResult + moduloHist + y);
@@ -243,30 +220,40 @@ public class CalculadoraView extends VerticalLayout {
                     var result = number + number2;
                     String s = String.valueOf(result);
                     resultadoTextField.add(s);
+                    moduloTextField.removeAll();
+                    moduloTextField.add("=");
                     break;
                 }
                 case "-": {
                     var result = number - number2;
                     String s = String.valueOf(result);
                     resultadoTextField.add(s);
+                    moduloTextField.removeAll();
+                    moduloTextField.add("=");
                     break;
                 }
                 case "X": {
                     var result = number * number2;
                     String s = String.valueOf(result);
                     resultadoTextField.add(s);
+                    moduloTextField.removeAll();
+                    moduloTextField.add("=");
                     break;
                 }
                 case "/": {
                     var result = number / number2;
                     String s = String.valueOf(result);
                     resultadoTextField.add(s);
+                    moduloTextField.removeAll();
+                    moduloTextField.add("=");
                     break;
                 }
                 case "%": {
                     var result = number % number2;
                     String s = String.valueOf(result);
                     resultadoTextField.add(s);
+                    moduloTextField.removeAll();
+                    moduloTextField.add("=");
                     break;
                 }
                 default:
